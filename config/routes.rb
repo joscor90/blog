6a9b6f2fae629c9841059bc1acb1dd2df3ce2a7d
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-10-21 17:34:13 -0300

    Deleted root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/dashboard'
   post 'posts/create'
-  root 'posts/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
