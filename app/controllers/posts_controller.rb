f71a0836912b621d399ed4f3de3331a1315a41d9
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-10-21 11:22:30 -0300

    defined create method in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,8 @@ class PostsController < ApplicationController
   def dashboard
     @post = Post.new
   end
+
+  def create 
+    @post = Post.new(title: params[:title], image_url: params[:image_url], content: params[:content])
+  end
 end
