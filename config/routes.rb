f42caa46ebc9e8766c36b38a45fb97dcad69d025
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-10-21 17:41:20 -0300

    Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/dashboard'
   post 'posts/create'
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
