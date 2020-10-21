546e9012be2dd7bba978e82e437f26ba48a3b164
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-10-21 16:43:45 -0300

    Added basic authentication to model instead of controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     validates :title, presence: true
     validates :image_url, presence: true
     validates :content, presence: true
+    http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only: :dashboard
 end
